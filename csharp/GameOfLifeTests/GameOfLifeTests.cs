9254c9ab3b38932c34ddacfc8d3ee5b4dcaaeaed
Brian Merchant <btmerchant@comcast.net>
2016-04-13 20:22:46 -0500

    Began the Tests

---
+++ csharp/GameOfLifeTests/GameOfLifeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class GameOfLifeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
